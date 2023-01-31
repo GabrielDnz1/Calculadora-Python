43189fa0a7b360578fbafe2d397e76837422d792
Gabriel Barbosa <gabrielbarbosa1315@gmail.com>
2023-01-30 21:25:39 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,15 @@
+print("Calculadora")
+n1 = int(input("Digite um número:"))
+n2 = int(input("Digite outro número:"))
+op = str(input("Digite um Operador(+, -, x, /):"))
+if op == "+":
+    print(n1 + n2)
+elif op == "-":
+    print(n1 - n2)
+elif op == "x":
+    print(n1 * n2)
+elif op == "/":
+    print(n1 / n2)
+
+
+
